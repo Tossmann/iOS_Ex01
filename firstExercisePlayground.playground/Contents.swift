418df04798733f25c68ba878a831cb978790cd5a
tossmann <ptrautma@XIN842949.asv.local>
2016-10-11 17:13:15 +0200

    Playground mit Beispielen bestückt

--- firstExercisePlayground.playground/Contents.swift
+++ firstExercisePlayground.playground/Contents.swift
@@ -8,5 +8,20 @@ let b = 10                          //Konstanten werden mit let versehen
 var c = a + b                       //Rechnen kann man damit trotzdem
 
 print("Hello, ", terminator:"")     //automatischen Zeilenumbruch am Ende des Strings - hier durch "" ersetzt
-print("world!")                     //dadurch steht das world in derselben Zeile
+print("world!")                     //dadurch steht das world in derselben Zeile // \n ist das Zeichen für den Zeilemumbruch --> siehe am rechten Rand
 
+var errCode: Int? = 404
+errCode = nil
+
+
+//klassische Variante -> nicht gut für SWIFT
+if errCode != nil{
+    print(errCode!)
+}
+
+//so soll es in Swift gemacht werden
+                                    //Beispiel für Optionals; Am besten kurz was dazu lesen und danach das Beispiel verstehen
+                                    //Optionals können einen Wert aus dem Datentyp annehmen oder eben nil (leere Menge)
+if let actualCode = errCode {       //Mit dem ! wie bei print(errCode!) wird gesagt, dass jetzt auf jeden Fall ein Integer Wert in errCode steht -> wenn du das falsch programmierst führt das beispielsweise zum Absturz
+    print(actualCode)
+}
