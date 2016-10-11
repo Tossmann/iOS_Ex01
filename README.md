0bbe6c81d584be116603d1b44e49887cf2c17641
tossmann <ptrautma@XIN842949.asv.local>
2016-10-11 16:46:14 +0200

    Playground erstellt mit ersten Kommentaren, lesen und ausprobieren.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # iOS_Ex01
+Playgrounds helfen dir beim probieren einzelner Codeschnipsel, also im Playground üben
---
+++ firstExercisePlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+print("Hello, world!")
+
+var a = 1
+a = a + 5                           //Variablen mit var - können geändert werden
+
+let b = 10                          //Konstanten werden mit let versehen
+
+var c = a + b                       //Rechnen kann man damit trotzdem
+
+print("Hello, ", terminator:"")     //automatischen Zeilenumbruch am Ende des Strings - hier durch "" ersetzt
+print("world!")                     //dadurch steht das world in derselben Zeile
+
